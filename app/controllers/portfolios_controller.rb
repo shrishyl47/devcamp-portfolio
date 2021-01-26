ece86abc3cd289d6f72a5b1c62b64a6dc365ec28
Shrishyl <shrishyl47@gmail.com>
2021-01-26 22:00:42 +0530

    Integrated delete action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,4 +37,12 @@ class PortfoliosController < ApplicationController
   	@portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+  	@portfolio_item = Portfolio.find(params[:id])
+  	@portfolio_item.destroy
+  	respond_to do |format|
+  		format.html {redirect_to portfolios_path, notice: 'portfolio has been removed'}
+  	end
+  end
+
 end
