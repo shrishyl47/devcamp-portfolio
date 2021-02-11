5b948680ffd1101f5cf33935db51d23f7240fd19
Shrishyl <shrishyl47@gmail.com>
2021-02-11 21:51:12 +0530

    Implemented custom action for updating status for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolios_show'
-  resources :blogs
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
