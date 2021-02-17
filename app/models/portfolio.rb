6bb44a8a249ec96e181dca07ef7b8107aaa1a755
Shrishyl <shrishyl47@gmail.com>
2021-02-17 16:54:24 +0530

    Implemented image default values in rails

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,4 +9,10 @@ class Portfolio < ApplicationRecord
 	# Another way to write custom scope using lambda
 	scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/600x400"
+		self.thumb_image ||= "https://via.placeholder.com/350x200"
+	end
 end
