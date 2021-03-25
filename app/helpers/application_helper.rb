0570c456d6a863a7a6f19aa330b574c349e06371
Shrishyl <shrishyl47@gmail.com>
2021-03-26 01:38:53 +0530

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,54 @@ module ApplicationHelper
 	def copyright_generator
 		ShrishylViewTool::Renderer.copyright 'Shrishyl V', 'All rights reserved'
 	end
+
+	def nav_items
+		# return an array of hashes
+		[
+			{
+				url: root_path,
+				title: 'Home'
+			},
+			{
+				url: about_path,
+				title: 'About Me'
+			},
+			{
+				url: contact_path,
+				title: 'Contact'
+			},
+			{
+				url: blogs_path,
+				title: 'Blog'
+			},
+			{
+				url: portfolios_path,
+				title: 'Portfolio'
+			}
+		]
+	end
+
+	def nav_generator css_style, tag_type
+		# Old way of code
+		#nav_links = <<NAV
+		#<#{tag_type}><a href="#{root_path}" class="#{css_style} #{active? root_path}">Home</a></#{tag_type}>
+		#<#{tag_type}><a href="#{about_path}" class="#{css_style} #{active? about_path}">About Me</a></#{tag_type}>
+		#<#{tag_type}><a href="#{contact_path}" class="#{css_style} #{active? contact_path}">Contact</a></#{tag_type}>
+		#<#{tag_type}><a href="#{blogs_path}" class="#{css_style} #{active? blogs_path}">Blog</a></#{tag_type}>
+		#<#{tag_type}><a href="#{portfolios_path}" class="#{css_style} #{active? portfolios_path}">Portfolio</a></#{tag_type}>
+		#NAV
+
+		# new way of code
+		nav_links = ''
+		nav_items.each do |item|
+			nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{css_style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+		end
+
+		nav_links.html_safe
+	end
+
+	def active? path
+		"active" if current_page? path
+		# return active if the current page(rails inbuilt method) matches the path we pass in.
+	end
 end
