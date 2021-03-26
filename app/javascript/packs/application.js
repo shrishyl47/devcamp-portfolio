0544210036730d26f0f0e372c4490a36e7961b84
Shrishyl <shrishyl47@gmail.com>
2021-03-26 22:25:26 +0530

    Installed jquery-ui, sortable html5 library and css calls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery-ui"
+import "html5sortable"
 
 Rails.start()
 Turbolinks.start()
