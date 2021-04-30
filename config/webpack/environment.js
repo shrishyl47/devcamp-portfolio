1f71e8a869fb4d352910d88e05d9c900ebc7e997
shrishyl <shrishyl47@gmail.com>
2021-04-30 21:30:31 +0530

    fixed jquery to display toast notifications

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,7 @@ const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
-    jQuery: 'jquery',
+    jQuery: 'jquery/src/jquery',
     'window.Tether': "tether",
     Popper: ['popper.js', 'default']
   })
