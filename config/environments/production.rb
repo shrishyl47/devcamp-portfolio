f47dbba9b5fc730fa65d661ca7b5f608c85fdc79
shrishyl <shrishyl47@gmail.com>
2021-05-18 17:07:48 +0530

    Integrated production call for action cable with some ui changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_cable.allowed_request_origins = ['https://sv-devcamp-portfolio.herokuapp.com', 'http://sv-devcamp-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://sv-devcamp-portfolio.herokuapp.com/cable"
 end
